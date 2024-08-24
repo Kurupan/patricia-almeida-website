9d1e06b848848760abb4e5a311e24df41aebee21
Savana Tezza Borges <108376116+Kurupan@users.noreply.github.com>
2024-08-24 15:35:14 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Botao menu Mobile
+
+function menuShow() {
+    if ( menuContent.style.display == 'block'){
+        menuContent.style.display = 'none'
+} else {
+    menuContent.style.display = 'block'
+}
+}
+
+
+
+// lightbox 2 custom
+
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'disableScrolling': true,
+  })
